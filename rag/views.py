92b40c47de9d292b4a55e73f487de535cf8e5298
Pratapkumar7992 <prtp.pndt123@outlook.com>
2026-09-14 11:58:57 +0530

    update with text extractor from pdf

--- rag/views.py
+++ rag/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Document
+from .services.pdf_loader import extract_text as extract_pdf_text
 
 
 def home(request):
+    extracted_text = ""
 
     if request.method == "POST":
         pdf = request.FILES.get("pdf")
 
         if pdf:
-            Document.objects.create(
+            document = Document.objects.create(
                 name=pdf.name,
                 file=pdf
             )
+            extracted_text = extract_pdf_text(document.file.path)
 
-    return render(request, "rag/index.html")
+    return render(request, "rag/index.html", {"extracted_text": extracted_text})
