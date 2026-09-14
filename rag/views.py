d185f4d4dc390f7ed6a15141f10cda03cf5f3974
Pratapkumar7992 <prtp.pndt123@outlook.com>
2026-09-14 11:00:58 +0530

    add upload document

--- rag/views.py
+++ rag/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Document
 
 
 def home(request):
+
+    if request.method == "POST":
+        pdf = request.FILES.get("pdf")
+
+        if pdf:
+            Document.objects.create(
+                name=pdf.name,
+                file=pdf
+            )
+
     return render(request, "rag/index.html")
